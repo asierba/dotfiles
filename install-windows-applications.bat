92051a8d106b3a438a68ddd09ecfe07eeb460c14
asierba <asierba@gmail.com>
2015-06-16 17:46:21 +0100

    Add bat script to install windows applications through chocolatey

---
+++ install-windows-applications.bat
@@ -0,0 +1,22 @@
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+choco install GoogleChrome 
+choco install sublimetext2
+choco install spotify
+choco install skype
+choco install NugetPackageExplorer
+choco install dropbox
+choco install googledrive
+choco install beyondcompare
+choco install Unlocker
+choco install uTorrent
+choco install vlc
+choco install poweriso
+choco install notepadplusplus
+choco install Console2
+choco install ruby
+choco install ruby.devkit
+choco install winrar
+choco install gitextensions
+choco install tomighty
+choco install lastpass
