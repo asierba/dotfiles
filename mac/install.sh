31caf7bc5c88708bc7501f433bc50dc3f1c5c86a
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2018-11-15 11:35:21 +0100

    Add script to install basic stuff in mac

--- mac/install.sh
+++ mac/install.sh
@@ -1,1 +1,31 @@
-## Install Mac apps & tools
+## Install Mac apps & tools 
+## Based on: https://github.com/SamirTalwar/fygm
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# shell
+brew install \
+  autojump \
+  wget \
+  zsh
+
+# development
+brew install \
+  git \
+  node
+
+# Java Development
+brew cask install java
+brew install \
+  gradle \
+  maven
+brew cask install intellij-idea
+
+brew cleanup
