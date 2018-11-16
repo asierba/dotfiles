bd2ebb2bb8e9c6750d5657cd23c5d47bc2742917
Asier <asier@Asiers-MacBook-Pro.local>
2018-11-16 11:20:14 +0100

    Remove windows folder and move mac contents to root
    Who cares about windows any more? ;)

--- windows/install.sh
+++
@@ -1,6 +0,0 @@
-## Run this script to copy files to appropiate places
-
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
-
-ln -s ~/.dotfiles/Console2/console.xml ~/AppData/Roaming/Console/console.xml
