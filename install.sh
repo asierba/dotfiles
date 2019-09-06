9e3a2a7dbed7514fee899a5a9102babc7db3d990
Asier Barrenetxea <asierba@gmail.com>
2019-09-06 14:54:56 +0200

    Add folder for custom script and restructure files

--- install.sh
+++ install.sh
@@ -1,49 +1,16 @@
-## Install Mac apps & tools 
-## Based on: https://github.com/SamirTalwar/fygm
+#!/bin/sh
 
-# Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing scripts and config files..."
 
-# Make sure we’re using the latest Homebrew.
-brew update
+echo ""
+echo "(Re)creating a symbolic link for each script to point to them from the ~ folder..."
+cd scripts
+ls * | xargs -I {}  ln -fs $(pwd)/{} /usr/local/bin/{} 
+cd -
 
-# Upgrade any already-installed formulae.
-brew upgrade
-
-# shell
-brew install \
-  z \
-  wget \
-  zsh \
-  watch \
-  tldr
-
-# development
-brew install \
-  git \
-  node \
-  ruby \
-  docker \ 
-  docker-compose \
-  heroku/brew/heroku \
-  Kubectl \ 
-  dbeaver-community
-brew cask install sublime-text
-
-# Java Development
-brew cask install java
-brew tap pivotal/tap
-brew install \
-  gradle \
-  maven \ 
-  springboot
-
-# Haskell
-brew install ghc
-
-# Mac stuff
-brew cask install spectacle 
-brew cask install beardedspice
-brew cask install caffeine
-
-brew cleanup
+if ! grep -q "source $(pwd)/.zshrc" ~/.zshrc; then # check if aliases have been imported already
+    echo "importing custom .zshrc in main .zshrc file.."
+    echo "backing up.. ~/.zshrc.bak"
+    cp ~/.zshrc ~/.zshrc.bak
+    echo "source $(pwd)/.zshrc" >> ~/.zshrc
+fi
