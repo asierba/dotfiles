dd635643c30d68855f0b9781f965ae8cc1a59fd5
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2019-09-06 14:54:56 +0200

    Add folder for custom script and restructure files

---
+++ install-dev-tools.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+## Install Mac apps & tools 
+## Based on: https://github.com/SamirTalwar/fygm
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# shell
+brew install \
+  z \
+  wget \
+  zsh \
+  watch \
+  tldr
+
+# development
+brew install \
+  git \
+  node \
+  ruby \
+  docker \ 
+  docker-compose \
+  heroku/brew/heroku \
+  kubectl \ 
+  dbeaver-community
+brew cask install sublime-text
+
+# Java Development
+brew cask install java
+brew tap pivotal/tap
+brew install \
+  gradle \
+  maven \ 
+  springboot
+
+# Haskell
+brew install ghc
+
+# Mac stuff
+brew cask install spectacle 
+brew cask install beardedspice
+brew cask install caffeine
+
+brew cleanup
